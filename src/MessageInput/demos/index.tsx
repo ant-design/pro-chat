f742db3edae279cfba8f34660d1aecadbb0adf3e
Shinji-Li <1349021570@qq.com>
2023-11-22 18:17:34 +0800

    feat：Highlight & Markdown 切换为 ProEditor (#12)
    * :sparkles: feat: change Highlight & Markdown into ProEditor
    
    * :sparkles: feat: delete lobeui
    
    * :green_heart: fix: ci fixed
    
    ---------
    
    Co-authored-by: ONLY-yours <1340921570@qq.com>

--- src/MessageInput/demos/index.tsx
+++ src/MessageInput/demos/index.tsx
@@ -1,6 +1,5 @@
+import { content } from '@/EditableMessage/demos';
 import { MessageInput } from '@ant-design/pro-chat';
-
-import { content } from '@/Markdown/demos/data';
 
 export default () => {
   return <MessageInput defaultValue={content} height={200} style={{ width: '100%' }} />;
