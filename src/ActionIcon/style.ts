9ecdac7e8108d695f3190e5b4583028ce0dae56e
ONLY-yours <1340921570@qq.com>
2023-11-27 10:50:13 +0800

    :bug: fix: fixed actionicon hover double mask

--- src/ActionIcon/style.ts
+++ src/ActionIcon/style.ts
@@ -1,7 +1,7 @@
 import { createStyles } from 'antd-style';
 
 export const useStyles = createStyles(
-  ({ css, token, stylish, cx }, { active, glass }: { active: boolean; glass: boolean }) => {
+  ({ css, stylish, cx }, { glass }: { active: boolean; glass: boolean }) => {
     return {
       block: cx(
         glass && stylish.blur,
@@ -14,34 +14,8 @@ export const useStyles = createStyles(
           flex: none;
           align-items: center;
           justify-content: center;
-
-          color: ${active ? token.colorText : token.colorTextTertiary};
-
-          background: ${active ? token.colorFillTertiary : 'transparent'};
-
-          transition:
-            color 600ms ${token.motionEaseOut},
-            scale 400ms ${token.motionEaseOut},
-            background-color 100ms ${token.motionEaseOut};
-
-          &:hover {
-            color: ${token.colorText};
-            background-color: ${token.colorFillSecondary};
-          }
-
-          &:active {
-            color: ${token.colorText};
-            background-color: ${token.colorFill};
-          }
         `,
       ),
-      icon: css`
-        transition: scale 400ms ${token.motionEaseOut};
-
-        &:active {
-          scale: 0.8;
-        }
-      `,
     };
   },
 );
