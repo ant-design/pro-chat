bf7c8d28032716fd3433ecc1e17be51933ccac9c
ONLY-yours <1349021570@qq.com>
2024-01-05 15:22:51 +0800

    :sparkles: feat: auto not work when render long stream markdown

--- src/ProChat/components/ScrollAnchor/index.tsx
+++ src/ProChat/components/ScrollAnchor/index.tsx
@@ -2,18 +2,23 @@ import { memo, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 import { useStore } from '@/ProChat/store';
+import { chatSelectors } from '../../store/selectors';
 
 import { useAtBottom } from './useAtBottom';
 
 const ChatScrollAnchor = memo(() => {
   const trackVisibility = useStore((s) => !!s.chatLoadingId);
+  const str = useStore(chatSelectors.currentChats);
 
-  const isAtBottom = useAtBottom();
   const { ref, entry, inView } = useInView({
     delay: 100,
     rootMargin: '0px 0px -150px 0px',
     trackVisibility,
   });
+
+  // 如果是移动端，可能200太多了，认为超过 1/3 即可，PC默认200
+  const ScrollOffset = window.innerHeight / 3 > 200 ? 200 : window.innerHeight / 4;
+  const isAtBottom = useAtBottom(ScrollOffset);
 
   useEffect(() => {
     if (isAtBottom && trackVisibility && !inView) {
@@ -21,7 +26,7 @@ const ChatScrollAnchor = memo(() => {
         block: 'start',
       });
     }
-  }, [inView, entry, isAtBottom, trackVisibility]);
+  }, [inView, entry, isAtBottom, trackVisibility, str]);
 
   return <div ref={ref} style={{ height: 1, width: '100%' }} />;
 });
