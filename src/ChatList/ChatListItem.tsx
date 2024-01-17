4444f404e6700cab76fe0d23d515dbd1272fac9d
Shinji-Li <1349021570@qq.com>
2024-01-17 15:25:42 +0800

    :bug: fix: 修复特殊情况下 avatar 配置不生效的问题 (#44)
    * :bug: fix: slove avatar meta was not work

--- src/ChatList/ChatListItem.tsx
+++ src/ChatList/ChatListItem.tsx
@@ -230,7 +230,7 @@ const Item = (props: ChatListItemProps) => {
       />
     );
     return dom;
-  }, [props.content, props.loading, props.id, item.updateAt || item.createAt]);
+  }, [props.content, props.loading, props.id, (item as any).meta, item.updateAt || item.createAt]);
 
   if (RenderItem) return <RenderItem key={item.id} {...props} />;
 
