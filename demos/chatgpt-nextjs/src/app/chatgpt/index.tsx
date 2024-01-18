9537df421a401a4a40378c5ae1bc5dd985f5bb4b
lkzwc <lkzwc@outlook.com>
2024-01-18 20:45:35 +0800

    fix: 修复next版本下首页渲染报错

--- demos/chatgpt-nextjs/src/app/chatgpt/index.tsx
+++ demos/chatgpt-nextjs/src/app/chatgpt/index.tsx
@@ -1,30 +1,38 @@
 'use client';
-
+import { useState, useEffect } from 'react'
 import { ProChat } from '@ant-design/pro-chat';
 import { useTheme } from 'antd-style';
 
 export default function Home() {
+
   const theme = useTheme();
+  const [showComponent, setShowComponent] = useState(false)
+
+  useEffect(() => setShowComponent(true), [])
+
   return (
     <div
       style={{
         backgroundColor: theme.colorBgLayout,
       }}
     >
-      <ProChat
-        style={{
-          height: '100vh',
-          width: '100vw',
-        }}
-        request={async (messages: Array<any>) => {
-          const response = await fetch('/api/openai', {
-            method: 'POST',
-            body: JSON.stringify({ messages: messages }),
-          });
-
-          return response;
-        }}
-      />
+      {
+        showComponent && <ProChat
+          style={{
+            height: '100vh',
+            width: '100vw',
+          }}
+          request={async (messages) => {
+            const response = await fetch('/api/qwen', {
+              method: 'POST',
+              body: JSON.stringify({ messages: messages }),
+            });
+            const data = await response.json();
+            return new Response(data.output?.text);
+          }}
+        />
+      }
     </div>
   );
 }
+
