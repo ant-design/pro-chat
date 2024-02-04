d0f0e82362396391f464d879fd6ac8abe53e743b
Shinji-Li <1349021570@qq.com>
2024-02-04 16:03:03 +0800

    fix(docs): 更新 ChatGPT 的示例文档内容  (#82)

--- demos/chatgpt-nextjs/src/app/chatgpt/index.tsx
+++ demos/chatgpt-nextjs/src/app/chatgpt/index.tsx
@@ -1,14 +1,13 @@
 'use client';
-import { useState, useEffect } from 'react'
 import { ProChat } from '@ant-design/pro-chat';
 import { useTheme } from 'antd-style';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-
   const theme = useTheme();
-  const [showComponent, setShowComponent] = useState(false)
+  const [showComponent, setShowComponent] = useState(false);
 
-  useEffect(() => setShowComponent(true), [])
+  useEffect(() => setShowComponent(true), []);
 
   return (
     <div
@@ -16,8 +15,8 @@ export default function Home() {
         backgroundColor: theme.colorBgLayout,
       }}
     >
-      {
-        showComponent && <ProChat
+      {showComponent && (
+        <ProChat
           style={{
             height: '100vh',
             width: '100vw',
@@ -27,12 +26,10 @@ export default function Home() {
               method: 'POST',
               body: JSON.stringify({ messages: messages }),
             });
-            const data = await response.json();
-            return new Response(data.output?.text);
+            return response;
           }}
         />
-      }
+      )}
     </div>
   );
 }
-
