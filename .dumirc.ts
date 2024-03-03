cb5e1d63bcc8d4439ffc1b9287a08ffc02f5882b
LCJove <851301775@qq.com>
2024-03-03 20:49:25 +0800

    :memo: docs: add en homepage

--- .dumirc.ts
+++ .dumirc.ts
@@ -12,72 +12,7 @@ export default defineConfig({
       github: 'https://github.com/ant-design/pro-chat',
     },
     footer: 'Made with ❤️ by 蚂蚁集团 - AFX & 数字科技',
-    features: [
-      {
-        image:
-          'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/q48YQ5X4ytAAAAAAAAAAAAAAFl94AQBr',
-        title: '简单易用',
-        description: '在 Ant Design 上进行了自己的封装，更加易用',
-      },
-      {
-        image: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-        title: 'Ant Design',
-        description: '与 Ant Design 设计体系一脉相承，无缝对接 antd 项目',
-      },
-      {
-        image:
-          'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/UKqDTIp55HYAAAAAAAAAAAAAFl94AQBr',
-        title: '大模型对话组件',
-        description: '内置对话模型常用的：数据编辑、重新发送、删除对话等这些默认的基本操作',
-      },
-
-      {
-        image:
-          'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/Y_NMQKxw7OgAAAAAAAAAAAAAFl94AQBr',
-        title: '预设样式',
-        description: '样式风格与 antd 一脉相承，无需魔改，浑然天成。默认好用的主题系统',
-      },
-      {
-        image:
-          'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/U3XjS5IA1tUAAAAAAAAAAAAAFl94AQBr',
-        title: 'AI Friendly 的数据结构',
-        description:
-          '参照 ChatGPT、GLM、通义千问等市面上主流的大模型入参出参，减少前端开发者对这些入参和出参的处理',
-      },
-
-      {
-        image: 'https://gw.alipayobjects.com/zos/antfincdn/Eb8IHpb9jE/Typescript_logo_2020.svg',
-        title: 'TypeScript',
-        description: '使用 TypeScript 开发，提供完整的类型定义文件，无需频繁打开官网',
-      },
-    ],
     logo: 'https://mdn.alipayobjects.com/huamei_re70wt/afts/img/A*Mo27Sr3kS4kAAAAAAAAAAAAADmuEAQ/original',
-    hero: {
-      'zh-CN': {
-        title: 'ProEditor',
-        description: '🌟 通用编辑器 UI 框架',
-        actions: [
-          {
-            text: '快速开始 →',
-            link: '/guide/intro-start',
-          },
-          {
-            text: 'Github',
-            link: 'https://github.com/ant-design/pro-chat',
-          },
-        ],
-      },
-      'en-US': {
-        title: 'ProEditor',
-        description: '🌟 A General Editor UI Framework',
-        actions: [
-          {
-            text: 'Quick Start →',
-            link: '/guide/intro-start',
-          },
-        ],
-      },
-    },
   },
   mfsu: false,
   outputPath: 'docs-dist',
---
+++ docs/index.en-US.md
@@ -0,0 +1,29 @@
+---
+hero:
+  title: ProChat
+  description: 🤖 use Chat Components like Pro!
+  actions:
+    - text: 'Quick Start →'
+      link: /en-US/guide/intro-start
+    - text: 'Github'
+      link: 'https://github.com/ant-design/pro-chat'
+features:
+  - image: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/q48YQ5X4ytAAAAAAAAAAAAAAFl94AQBr'
+    title: 'Simple and easy-to-use'
+    description: 'Encapsulated on Ant Design, making it easier to use'
+  - image: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg'
+    title: 'Ant Design'
+    description: 'Consistent with the Ant Design design system, seamlessly integrating with ant projects'
+  - image: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/UKqDTIp55HYAAAAAAAAAAAAAFl94AQBr'
+    title: 'Large model dialogue component'
+    description: 'Common default basic operations of built-in dialogue models include data editing, resending, deleting conversations, etc'
+  - image: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/Y_NMQKxw7OgAAAAAAAAAAAAAFl94AQBr'
+    title: 'Preset styles'
+    description: 'The style and style are in line with Antd, without the need for magic modification, and are naturally created. Default and user-friendly theme system'
+  - image: 'https://mdn.alipayobjects.com/yuyan_qk0oxh/afts/img/U3XjS5IA1tUAAAAAAAAAAAAAFl94AQBr'
+    title: 'The data structure of AI Friendly'
+    description: 'Referring to mainstream large models such as ChatGPT, GLM, and Tongyi Qianwen on the market for input and output parameters, reduce the processing of these input and output parameters by front-end developers'
+  - image: 'https://gw.alipayobjects.com/zos/antfincdn/Eb8IHpb9jE/Typescript_logo_2020.svg'
+    title: 'TypeScript'
+    description: 'Developed using TypeScript, providing complete type definition files without frequent opening of official websites'
+---
