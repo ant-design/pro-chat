c668f9a84b3f1812dfd85a153a76af6af63a9fca
gaoyizhuo.gyz <gaoyizhuo.gyz@antgroup.com>
2024-05-10 13:59:00 +0800

    :globe_with_meridians: chore: add language

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,14 +1,16 @@
 import { LocaleProps } from '@/types/locale';
 import enUSLocal from './en-US';
 import zhCNLocal from './zh-CN';
-export type Locale = 'zh-CN' | 'en-US';
+import zhHKLocal from './zh-HK';
+export type Locale = 'zh-CN' | 'en-US' | 'zh-HK';
 
 const locales = {
   'en-US': enUSLocal,
   'zh-CN': zhCNLocal,
+  'zh-HK': zhHKLocal,
   en: enUSLocal,
 };
 
-export const gLocaleObject = (glocale: Locale): LocaleProps => {
-  return locales[glocale as 'zh-CN'] || locales['zh-CN'];
+export const gLocaleObject = (gLocale: Locale): LocaleProps => {
+  return locales[gLocale as 'zh-CN'] || locales['zh-CN'];
 };
