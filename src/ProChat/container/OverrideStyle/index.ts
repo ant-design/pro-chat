9d80fa8cf87ff26a81bed67c2826f1a231abcee1
gin-lsl <gin_lsl@outlook.com>
2024-05-30 20:00:23 +0800

    :lipstick: style: move all global styles under .ant-pro-chat

--- src/ProChat/container/OverrideStyle/index.ts
+++ src/ProChat/container/OverrideStyle/index.ts
@@ -1,6 +1,10 @@
 import { createStyles } from 'antd-style';
 import global from './global';
 
-export const useOverrideStyles = createStyles(({ token, prefixCls, cx }) => ({
-  container: cx(`${prefixCls}-pro-chat`, global(token)),
-}));
+export const useOverrideStyles = createStyles(({ token, prefixCls, cx }) => {
+  const rootClassName = `${prefixCls}-pro-chat`;
+
+  return {
+    container: cx(rootClassName, global(token, rootClassName)),
+  };
+});
