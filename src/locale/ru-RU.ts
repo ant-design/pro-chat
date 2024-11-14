44326e690417bce75fc0176a6f070ff597407a68
gelonsoft <gelonsoft@gmail.com>
2024-11-14 23:58:31 +0300

    Create ru-RU.ts
    Add ru-RU

---
+++ src/locale/ru-RU.ts
@@ -0,0 +1,16 @@
+export default {
+  placeholder: 'Введите сообщение...',
+  backToBottom: 'Вернуться вниз',
+  clearCurrentDialogue: 'Очистить текущий диалог',
+  clearDialogue: 'Очистить диалог',
+  clearModalTitle:  'Вы собираетесь очистить сеанс, и вы не сможете восстановить его после очистки. Хотите очистить текущий сеанс?',
+  defaultHelloMessage: 'Привет! Начнем чат',
+  cancel: 'Отмена',
+  confirmed: 'Подтвердить',
+  copy: 'Копировать',
+  copySuccess: 'Копировать успешно',
+  delete: 'Удалить',
+  edit: 'Изменить',
+  history: 'История',
+  regenerate: 'Восстановить',
+};
